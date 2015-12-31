8c1005c00a5443b6d70db2cb0d320522a47431a1
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2015-12-31 15:25:37 +0100

    refactor(config elts): use lombok and an id for each template

--- src/main/java/com/github/nodevops/confd/maven/plugin/model/TemplateConfig.java
+++ src/main/java/com/github/nodevops/confd/maven/plugin/model/TemplateConfig.java
@@ -1,53 +1,18 @@
 package com.github.nodevops.confd.maven.plugin.model;
 
+import lombok.Data;
+
 import java.io.File;
 import java.util.Arrays;
 
 /**
  * Created by pseillier on 21/12/2015.
  */
+@Data
 public class TemplateConfig {
+    public static final String DEFAULT_ID = "undef";
+    private String id = DEFAULT_ID;
     private File src;
     private File dest;
-
     private String[] keys;
-
-    public File getSrc() {
-
-        return src;
-    }
-
-    public File getDest() {
-
-        return dest;
-    }
-
-    public String[] getKeys() {
-
-        return keys;
-    }
-
-    public void setSrc(File src) {
-
-        this.src = src;
-    }
-
-    public void setDest(File dest) {
-
-        this.dest = dest;
-    }
-
-    public void setKeys(String[] keys) {
-
-        this.keys = keys;
-    }
-
-    @Override
-    public String toString() {
-        return "TemplateConfig{" +
-            "src='" + src + '\'' +
-            ", dest='" + dest + '\'' +
-            ", keys=" + Arrays.toString(keys) +
-            '}';
-    }
 }
