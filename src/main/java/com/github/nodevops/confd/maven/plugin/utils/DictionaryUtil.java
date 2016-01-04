288b765dae8c37f8f49b01ec43f03bbc99b86641
Seillier Philippe <pseillier@gmail.com>
2016-01-04 08:31:54 +0100

    Fix : replace FileUtils.split with String.split to handle correctly blank characters around the '=' separator. Optimize regular expression

--- src/main/java/com/github/nodevops/confd/maven/plugin/utils/DictionaryUtil.java
+++ src/main/java/com/github/nodevops/confd/maven/plugin/utils/DictionaryUtil.java
@@ -11,9 +11,39 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
- * Created by pseillier on 28/12/2015.
+ * Contains some helper methods to handle dictionaries. A dictionary is a file containing key/value pairs.
+ * The file may contain comments which begin with a '#'.
+ * The key/value separator is the '=' character.
+ * The key format is :
+ * - a sequence of parts
+ * - a part begin with a "/" character followed by one or more uppercase letter
+ * and/or lowercase letter and/or digit and or underscore character "_"
+ * <p>
+ * Example of valid key/value pairs :
+ * <p>
+ * <pre>
+ *
+ * /web/media_server/address = media.server.com
+ * /web/home_page = http://www.home.com/
+ * /proxy/http = server.proxy.int
+ * /proxy/https = server.proxy.int
+ * /users/user01 = user1
+ * /user/use02 = user2
+ *
+ * </pre>
  */
 public class DictionaryUtil {
+    /**
+     * Load the <code>dictionary</code> file into a <code>Map</code> according to the <code>encoding</code>.
+     * <p>
+     * The key names are converted to shell environment variable format.
+     * For instance /part1/part2 is converted to PART1_PART2 (the first '/' is removed).
+     *
+     * @param dictionary the file path to load
+     * @param encoding   the encoding of the file to load
+     * @return a <code>Map</code> representation of the file
+     * @throws DictionaryException if the file cannot be load or if a key/value is not valid.
+     */
     public static Map<String, String> readDictionaryAsEnvVariables(File dictionary, String encoding) throws DictionaryException {
 
         try {
@@ -24,8 +54,12 @@ public class DictionaryUtil {
             }
             Map<String, String> env = new HashMap<String, String>();
             for (String line : lines) {
-                String[] split = StringUtils.split(line, "=", 2);
-                if (split.length < 2) {
+                String[] split = line.split("\\s*=\\s*", 2);
+
+                if (split.length != 2) {
+                    throw new DictionaryException("dictionary " + dictionary + " : bad key/value format for " + line);
+                }
+                if (StringUtils.isEmpty(split[1])) {
                     throw new DictionaryException("dictionary " + dictionary + " : bad key/value format for " + line);
                 }
                 env.put(normalizeKey(split[0]), split[1]);
@@ -43,7 +77,7 @@ public class DictionaryUtil {
     /**
      * Convert a key from confd format "/part1/part2/...." to  shell variable format "PART1_PART2_....".
      * The valid format for the input key is :
-     * <code>(/[a-zA-Z0-9]+)+</code>
+     * <code>(/\w+)+</code>
      *
      * @param key the key to convert
      * @return a new key in shell variable format
@@ -52,7 +86,7 @@ public class DictionaryUtil {
     private static String normalizeKey(String key) throws InvalidKeyFormatException {
 
         // check the key format
-        if (!Pattern.matches("(/[A-Za-z0-9]+)+", key)) {
+        if (!Pattern.matches("(/\\w+)+", key)) {
             throw new InvalidKeyFormatException("the key " + key + " format is not valid. Valid format is (/[a-zA-Z0-9]+)+");
         }
         // convert confd key format /part1/part2/.../... to shell variable PART1_PART2_..._...
