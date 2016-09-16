63b49ee94194a16b7ae545a3d771a4ec126a5348
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2016-09-16 23:01:57 +0200

    fix(ci): run every parts of the build inside docker

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -88,12 +88,12 @@ function check_travis_branch_equals_travis_tag(){
 
 function publish_docs() {
   echo "[Publishing Docs] Publishing..."
-  mvn site -P publish-site --settings maven-settings.xml $MVN_OPTS
+  docker run --rm -ti -e CI_GITHUB_USERNAME -e CI_GITHUB_PASSWORD -e CI_JFROG_USERNAME -e CI_JFROG_PASSWORD -e JAVA_HOME=/usr/lib/jvm/java-1.7-openjdk -v $(pwd):/test -v $HOME/.m2:/test/.m2 nodevops/maven-fury-confd:1.0 /opt/local/maven-3.1.x/bin/mvn site -P publish-site --settings maven-settings.xml -Dconfd.local.path.for.tests=/opt/local/confd-0.11.0/confd
   echo "[Publishing Docs] Done"
 }
 function publish_snapshot(){
   echo "[Publishing Snapshot] Publishing..."
-  mvn clean deploy --settings maven-settings.xml $MVN_OPTS
+  docker run --rm -ti -e CI_GITHUB_USERNAME -e CI_GITHUB_PASSWORD -e CI_JFROG_USERNAME -e CI_JFROG_PASSWORD -e JAVA_HOME=/usr/lib/jvm/java-1.7-openjdk -v $(pwd):/test -v $HOME/.m2:/test/.m2 nodevops/maven-fury-confd:1.0 /opt/local/maven-3.1.x/bin/mvn clean deploy --settings maven-settings.xml -Dconfd.local.path.for.tests=/opt/local/confd-0.11.0/confd
   echo "[Publishing Snapshot] Done"
 }
 
